646617e9cf036c99a3b4fca92571c0a1cfff797a
Kirill Fedoseev <k.fedoseev@innopolis.university>
2022-07-15 15:31:09 +0400

    Transfer bridged erc20 back

--- demo/scripts/send_home_to_foreign.sh
+++ demo/scripts/send_home_to_foreign.sh
@@ -5,5 +5,10 @@ set -e
 source ./vars/vars.env
 source ./vars/contracts.env
 
+#cast send --keystore $(pwd)/vars/keys/key_user.json --password '' --rpc-url $HOME_RPC_URL --gas 1000000 \
+#  --value 1ether $HOME_ROUTER 'wrapAndRelayTokens()'
+
+HOME_BRIDGED_WETH=$(cast call --rpc-url $HOME_RPC_URL $HOME_OB 'bridgedTokenAddress(address) (address)' $FOREIGN_WETH)
+
 cast send --keystore $(pwd)/vars/keys/key_user.json --password '' --rpc-url $HOME_RPC_URL --gas 1000000 \
-  --value 1ether $HOME_ROUTER 'wrapAndRelayTokens()'
+  $HOME_BRIDGED_WETH 'transfer(address,uint256)' $HOME_OB $(cast to-unit 1ether)
